9de99bafa3dfc6ba0ea614b96ba461b44b8b44ed
Micah Kwaka <micahkwaka@gmail.com>
2024-12-13 16:47:54 -0500

    add isbn logic

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,13 +13,19 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  if (req.params.isbn) {
+    const isbn = req.params.isbn;
+    const getBook = books[isbn];
+    return res.send(JSON.stringify(getBook, null, 4));
+  } else {
+    return res.status(403).json({message: "This book does not exist"})
+  }
  });
   
 // Get book details based on author
